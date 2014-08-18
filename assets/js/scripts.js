2eb51de8fe69ca7357ba5f2dc8cb0deaec3b71f0
Kyle Knight <dreamsynk@gmail.com>
2014-08-18 09:21:34 +0200

    Published all files

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+$(window).load(function() {
+  jQuery(".social-box").delay(3500).queue(function(){$(this).addClass('moveIn')});
+  jQuery(".pt-triggers").delay(4000).queue(function(){$(this).addClass('fadeIn')});
+});
