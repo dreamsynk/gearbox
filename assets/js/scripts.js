ab808a131a965a593d55c5eb47a605462a83d8cf
Kyle Knight <dreamsynk@gmail.com>
2014-08-20 17:55:21 +0200

    Added Tooltips

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,32 @@
-$(window).load(function() {
-  jQuery(".social-box").delay(3500).queue(function(){$(this).addClass('moveIn')});
-  jQuery(".pt-triggers").delay(4000).queue(function(){$(this).addClass('fadeIn')});
+jQuery(window).load(function() {
+	
+	// Loader content delay
+  jQuery(".social-box").delay(3500).queue(function(){
+		$(this).addClass('moveIn')
+	});
+  jQuery(".pt-triggers").delay(4000).queue(function(){
+		$(this).addClass('fadeIn')
+	});
+	
+	// Tooltips
+setTimeout(function(){
+	
+	jQuery('#tooltips').tourbus({
+	autoDepart: true,
+  onLegStart: function( leg, bus ) {
+    if( leg.rawData.highlight ) {
+      leg.$target.addClass('my-tour-highlight');
+      $('.my-tour-overlay').show();
+    }
+  },
+  onLegEnd: function( leg, bus ) {
+    if( leg.rawData.highlight ) {
+      leg.$target.removeClass('my-tour-highlight');
+      $('.my-tour-overlay').hide();
+    }
+  }
+	});
+	}, 5000);
+	
 });
+
